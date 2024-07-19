8889dc59739ce4f7dcc921720779507d448a5855
Szebasztián Turbucz <154126262+Szebi60@users.noreply.github.com>
2024-07-19 12:42:05 +0200

    Add files via upload

---
+++ 10j.js
@@ -0,0 +1,20 @@
+let calculation = localStorage.getItem('calculation') || '';
+
+        displayCalculation();      
+      
+      function updateCalculation(button) {
+        calculation += button;
+
+        displayCalculation();
+
+        saveCalculation();
+      }
+
+      function saveCalculation() {
+        localStorage.setItem('calculation', calculation);
+      }
+
+      function displayCalculation() {
+        document.querySelector('.js-calculation')
+        .innerHTML = calculation;
+      }
